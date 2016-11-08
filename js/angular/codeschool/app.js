0afb6d01c66260a57eb6c0885f36c241d72279b7
emilyeserven <emily.e.serven@gmail.com>
2016-11-08 13:57:23 -0500

    Finished CodeSchool lesson 5.2

--- js/angular/codeschool/app.js
+++ js/angular/codeschool/app.js
@@ -1,22 +1,8 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('StoreController', function(){
     this.products = gems;
-  });
-
-  app.directive('productGallery', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-gallery.html',
-      controller: function() {
-        this.current = 0;
-        this.setCurrent = function(newGallery){
-          this.current = newGallery || 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
   });
 
   app.controller('ReviewController', function(){
@@ -25,44 +11,6 @@
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review={};
-    };
-  });
-
-  app.directive('productDescription', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-description.html'
-    };
-  });
-
-  app.directive('productReviews', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-reviews.html'
-    };
-  });
-
-  app.directive('productSpecs', function(){
-    return {
-      restrict: 'A',
-      templateUrl: 'product-specs.html'
-    };
-  });
-
-  app.directive('productTabs', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-tabs.html',
-      controller: function(){
-        this.tab = 1;
-        this.setTab = function(selectedTab) {
-          this.tab = selectedTab;
-        };
-        this.isSet = function(checkTab) {
-          return this.tab === checkTab;
-        };
-      },
-      controllerAs: 'tab'
     };
   });
 
