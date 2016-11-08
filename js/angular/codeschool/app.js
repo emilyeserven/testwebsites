272973004fef86df79305cfe7afa29901398f310
emilyeserven <emily.e.serven@gmail.com>
2016-11-07 21:10:54 -0500

    Finished CodeSchool lesson 3.6

--- js/angular/codeschool/app.js
+++ js/angular/codeschool/app.js
@@ -1,13 +1,6 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function() {
-    this.current = 0;
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    }
-  });
-
   app.controller('StoreController', function(){
     this.products = gems;
   });
@@ -20,6 +13,21 @@
     this.isSet = function(checkTab) {
       return this.tab === checkTab;
     };
+  });
+
+  app.controller('GalleryController', function() {
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    }
+  });
+
+  app.controller('ReviewController', function(){
+      this.review = {};
+      this.addReview = function(product){
+          product.reviews.push(this.review);
+          this.review={};
+      };
   });
 
   var gems = [{
