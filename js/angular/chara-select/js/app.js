e29664d2ba2c9055505f69c6543aeb589fb52a56
emilyeserven <emily.e.serven@gmail.com>
2016-11-09 15:52:10 -0500

    Added custom directive and controllers for character displays.

--- js/angular/chara-select/js/app.js
+++ js/angular/chara-select/js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module("charaSelect", []);
+(function() {
+    var app = angular.module("charaSelect", []);
+
+    app.directive('charaDisplay', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'chara-display.html',
+            controller: ['$http', function($http) {
+                var character = this;
+                character.stats = [];
+                $http.get('js/characters.json').success(function(data){
+                    character.stats = data;
+                });
+            }],
+            controllerAs: 'characters'
+        };
+    });
+
+})();
