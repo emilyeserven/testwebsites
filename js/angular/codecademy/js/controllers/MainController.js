cad4c511786f971e70621375ffe73c7823e9b757
emilyeserven <emily.e.serven@gmail.com>
2016-11-08 21:51:31 -0500

    Finished Codecademy Angular Lesson 3

--- js/angular/codecademy/js/controllers/MainController.js
+++ js/angular/codecademy/js/controllers/MainController.js
@@ -1,4 +1,8 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Top Sellers in Books';
   $scope.promo = 'Read more now!';
+  $scope.product = {
+    name: 'The Book of Trees',
+    price: 19
+  };
 }]);
