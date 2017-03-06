99d36f4f74d441b275cfc9d8b5638881de62f72e
emilyeserven <emily.e.serven@gmail.com>
2017-03-05 19:22:52 -0500

    Added more form fields

--- php/codeschool/exercises.php
+++ php/codeschool/exercises.php
@@ -1,5 +1,9 @@
 <?php
   $city = 'Tokyo';
+  $country = 'Japan';
+  $established = 1868;
+  $largest_population = true;
+  $population = 13613660;
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,9 +18,14 @@
         <div class="info">
           <ul>
             <li><?php echo $city; ?></li>
+            <li><?php echo $country; ?></li>
+            <li><?php echo $established; ?></li>
+            <li>Population: <?php echo $population; ?></li>
           </ul>
         </div>
         <div class="details">
+          <p><?php echo $city . ' has a very high population'; ?></p>
+          <p><?php echo "$city is the capital of $country and was established in $established." ?></p>
           <p>It officially became the capital after Emperor Meiji
             moved his seat to the city from the old capital of
             Kyoto.</p>
