17625f27fb4acc07742a89edb74a4e4037f8ce78
emilyeserven <emily.e.serven@gmail.com>
2017-03-05 19:45:10 -0500

    Updated to be able to use partials.

--- js/angular/lynda/js/controllers.js
+++ js/angular/lynda/js/controllers.js
@@ -1,5 +1,6 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+var artistControllers = angular.module('artistControllers', []);
+
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.artistOrder = 'name';
