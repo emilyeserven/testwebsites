6f8b34c1e45b08f55eae3bd1baa6a2cb99439658
emilyeserven <emily.e.serven@gmail.com>
2018-02-12 09:59:25 -0500

    Added some pizzazz and SCSS

--- js/animejs/surprise/app.js
+++ js/animejs/surprise/app.js
@@ -1,10 +1,28 @@
 var maincircAnim = anime({
-  targets: '#cssSelector .maincircle',
+  targets: '#cssSelector .heart.origheart',
   scale: 70,
-  duration: 2000,
-  backgroundColor: '#B60F0B',
-  opacity: .9
+  duration: 3500,
+  opacity: .9,
+  easing: 'easeInOutSine'
 });
+var maincircAnimFill = anime({
+  targets: '#cssSelector .heart.fillheart',
+  scale: 70,
+  duration: 3000,
+  offset: 1000,
+  opacity: .9,
+  easing: 'easeInOutSine'
+});
+
+var lineDraw = anime({
+  targets: '.heart.orig path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 2500,
+  delay: function(el, i) { return i * 250 }
+});
+
+
 var textAnim = anime({
   targets: '.flextest .text',
   scale: [
@@ -13,38 +31,97 @@ var textAnim = anime({
   ],
   color: [
     {value: '#940400', duration: 0},
-    {value: '#EEEEEE', duration: 1500, delay: 500}
+    {value: '#FFFFFF', duration: 1500, delay: 500}
   ],
   opacity: [
     {value: 0, duration: 0},
     {value: 1, duration: 1500, delay: 500}
   ],
+  offset: 1500,
+  easing: 'easeInOutSine'
 });
-var beginAnim = anime({
-  targets: '#cssSelector .circle.begin',
-  translateY: '-60vh',
+
+
+var circ1Anim = anime({
+  targets: '#cssSelector .circ1.orig',
+  translateY: '-70vh',
   duration: 3000,
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
   easing: 'easeInQuad'
 });
-var begincontAnim = anime({
-  targets: '#cssSelector .circle.begincont',
-  translateY: '-110vh',
-  duration: 3500,
-  offset: 3000,
+var circ1contAnim = anime({
+  targets: '#cssSelector .circ1.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 4500, delay: 2000}
+  ],
   loop: true,
   easing: 'easeInQuad'
 });
 var circ2Anim = anime({
-  targets: '#cssSelector .circle.circ2',
+  targets: '#cssSelector .circ2.orig',
   translateY: '-90vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
   duration: 3000,
   easing: 'easeInQuad'
 });
 var circ2contAnim = anime({
-  targets: '#cssSelector .circle.circ2cont',
-  translateY: '-110vh',
-  duration: 5000,
-  offset: 3000,
+  targets: '#cssSelector .circ2.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 2000}
+  ],
+  loop: true,
+  easing: 'easeInQuad'
+});
+
+
+var circ3Anim = anime({
+  targets: '#cssSelector .circ3.orig',
+  translateY: '-30vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
+  duration: 3000,
+  easing: 'easeInQuad'
+});
+var circ3contAnim = anime({
+  targets: '#cssSelector .circ3.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 2000}
+  ],
+  loop: true,
+  easing: 'easeInQuad'
+});
+
+var circ4Anim = anime({
+  targets: '#cssSelector .circ4.orig',
+  translateY: '-100vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
+  duration: 6000,
+  easing: 'easeInQuad'
+});
+var circ4contAnim = anime({
+  targets: '#cssSelector .circ4.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 5000}
+  ],
   loop: true,
   easing: 'easeInQuad'
 });
