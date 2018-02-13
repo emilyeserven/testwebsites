fcedbbf1f49e7a9eb11e7a278954f0fd885689f3
emilyeserven <emily.e.serven@gmail.com>
2018-02-12 19:15:12 -0500

    More bubbles and font switch

--- js/animejs/surprise/app.js
+++ js/animejs/surprise/app.js
@@ -15,7 +15,7 @@ var maincircAnimFill = anime({
 });
 
 var lineDraw = anime({
-  targets: '.heart.orig path',
+  targets: '.heart.origheart path',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
   duration: 2500,
@@ -67,8 +67,8 @@ var circ2Anim = anime({
   translateY: '-90vh',
   opacity: [
     {value: 1, duration: 0},
-    {value: 1, duration: 0, delay: 2500},
-    {value: 0, duration: 500, delay: 2501}
+    {value: 1, duration: 0, delay: 2250},
+    {value: 0, duration: 500, delay:2251}
   ],
   duration: 3000,
   easing: 'easeInQuad'
@@ -77,7 +77,7 @@ var circ2contAnim = anime({
   targets: '#cssSelector .circ2.circcont',
   translateY: [
     {value: '0vh', duration: 0},
-    {value: '-120vh', duration: 5000, delay: 2000}
+    {value: '-120vh', duration: 5000, delay: 1800}
   ],
   loop: true,
   easing: 'easeInQuad'
@@ -92,14 +92,14 @@ var circ3Anim = anime({
     {value: 1, duration: 0, delay: 2500},
     {value: 0, duration: 500, delay: 2501}
   ],
-  duration: 3000,
+  duration: 2000,
   easing: 'easeInQuad'
 });
 var circ3contAnim = anime({
   targets: '#cssSelector .circ3.circcont',
   translateY: [
     {value: '0vh', duration: 0},
-    {value: '-120vh', duration: 5000, delay: 2000}
+    {value: '-120vh', duration: 5000, delay: 1500}
   ],
   loop: true,
   easing: 'easeInQuad'
@@ -123,6 +123,114 @@ var circ4contAnim = anime({
     {value: '-120vh', duration: 5000, delay: 5000}
   ],
   loop: true,
+  easing: 'easeInQuad'
+});
+
+var circ5Anim = anime({
+  targets: '#cssSelector .circ5.orig',
+  translateY: '-85vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
+  scale: [
+    {value: 0, duration: 0},
+    {value: 1, duration: 1500, delay: 100},
+  ],
+  duration: 4000,
+  offset: 3000,
+  easing: 'easeInQuad'
+});
+var circ5contAnim = anime({
+  targets: '#cssSelector .circ5.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 2500}
+  ],
+  loop: true,
+  offset: 5000,
+  easing: 'easeInQuad'
+});
+
+var circ6Anim = anime({
+  targets: '#cssSelector .circ6.orig',
+  translateY: '-35vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
+  scale: [
+    {value: 0, duration: 0},
+    {value: 1, duration: 1500, delay: 100},
+  ],
+  duration: 4000,
+  offset: 1000,
+  easing: 'easeInQuad'
+});
+var circ6contAnim = anime({
+  targets: '#cssSelector .circ6.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 2500}
+  ],
+  loop: true,
+  offset: 3000,
+  easing: 'easeInQuad'
+});
+
+var circ7Anim = anime({
+  targets: '#cssSelector .circ7.orig',
+  translateY: '-60vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
+  scale: [
+    {value: 0, duration: 0},
+    {value: 1, duration: 1500, delay: 100},
+  ],
+  duration: 4000,
+  offset: 2000,
+  easing: 'easeInQuad'
+});
+var circ7contAnim = anime({
+  targets: '#cssSelector .circ7.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 2500}
+  ],
+  loop: true,
+  offset: 4000,
+  easing: 'easeInQuad'
+});
+
+var circ8Anim = anime({
+  targets: '#cssSelector .circ8.orig',
+  translateY: '-50vh',
+  opacity: [
+    {value: 1, duration: 0},
+    {value: 1, duration: 0, delay: 2500},
+    {value: 0, duration: 500, delay: 2501}
+  ],
+  scale: [
+    {value: 0, duration: 0},
+    {value: 1, duration: 1500, delay: 100},
+  ],
+  duration: 4000,
+  offset: 6000,
+  easing: 'easeInQuad'
+});
+var circ8contAnim = anime({
+  targets: '#cssSelector .circ8.circcont',
+  translateY: [
+    {value: '0vh', duration: 0},
+    {value: '-120vh', duration: 5000, delay: 2500}
+  ],
+  loop: true,
+  offset: 8000,
   easing: 'easeInQuad'
 });
 console.log("wow");
