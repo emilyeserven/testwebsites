29423dd95a6b71246b25054a97baa695a057c94d
emilyeserven <emily.e.serven@gmail.com>
2018-03-08 18:32:30 -0500

    Added final nodejs lynda code

--- js/nodejs/lynpractice/mainproject/server.js
+++ js/nodejs/lynpractice/mainproject/server.js
@@ -3,28 +3,64 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+mongoose.Proimse = Promise
 
-app.get('/messages', (req, res) =>{
-    res.send(messages)
+var dbUrl = 'mongodb://usertesting:Pass12word@ds259778.mlab.com:59778/learning-node'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
+app.get('/messages', (req, res) =>{
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.get('/messages/:user', (req, res) =>{
+    var user = req.params.user
+    Message.find({name:user}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) =>{
+    var message = new Message(req.body)
+
+    var savedMessagee = await message.save()
+
+    console.log('saved')
+
+    var censored = await Message.findOne({message: 'badword'})
+
+    if(censored)
+        await Message.remove({_id: censored.id})
+    else
+        io.emit('message', req.body)
+
     res.sendStatus(200)
+
+    /*
+    .catch((err) => {
+        res.sendStatus(500)
+        return console.error(err)
+    })
+    */
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection' ,err)
 })
 
 var server = http.listen(3000, () => {
